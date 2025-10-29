185887fa7f37c8d10d026360743e46f913847dce
wayzeywakeup <1wayzeywakeup@gmail.com>
2025-10-29 13:22:56 +0300

    feat: add database initialization

--- db.go
+++ db.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "modernc.org/sqlite"
+)
+
+func initDB() {
+	database, err := sql.Open("sqlite", "./task.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
+
+	statement, err := database.Prepare(
+		`CREATE TABLE IF NOT EXISTS task(
+			id INTEGER PRIMARY KEY,
+			title TEXT,
+			done BOOLEAN
+		)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = statement.Exec()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Database initialized successfully")
+}
